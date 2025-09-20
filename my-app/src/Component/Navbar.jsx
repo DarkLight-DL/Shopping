d0264c514a21e72a0651e63bb4e1b290ef3893ee
DarkLight-DL <a.asikahamedm@gmail.com>
2025-09-20 16:57:34 +0530

    button

--- my-app/src/Component/Navbar.jsx
+++ my-app/src/Component/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             component={Link}
             to='/'
             onClick={scrollToTop}
-            sx={{ p:0.1,py:0.5,minWidth: '10px', bgcolor: 'rgba(77, 73, 73, 0.59)', boxShadow: '0px 5px 10px black', '&:hover': { bgcolor: 'rgba(40, 39, 39, 0.1)' } }}
+            sx={{ p:0.1,py:0.5,minWidth: '10px',pr:{xs:0,sm:0,md:0,lg:1}, bgcolor: 'rgba(77, 73, 73, 0.59)', boxShadow: '0px 5px 10px black', '&:hover': { bgcolor: 'rgba(40, 39, 39, 0.1)' } }}
           >
             <HomeIcon sx={{ p: 0, mr: "5px", pl: '5px' }} />
             <Typography sx={{ p: 0, display: { xs: 'none', sm: 'none', lg: 'block' } }}>
@@ -69,7 +69,7 @@ const Navbar = () => {
             component={Link}
             to='/wishlist'
             onClick={scrollToTop}
-            sx={{p:0.1,py:0.5, minWidth: '10px', bgcolor: 'rgba(77, 73, 73, 0.59)', boxShadow: '0px 5px 10px black', '&:hover': { bgcolor: 'rgba(40, 39, 39, 0.1)' } }}
+            sx={{p:0.1,py:0.5, minWidth: '10px',pr:{xs:0,sm:0,md:0,lg:1}, bgcolor: 'rgba(77, 73, 73, 0.59)', boxShadow: '0px 5px 10px black', '&:hover': { bgcolor: 'rgba(40, 39, 39, 0.1)' } }}
             startIcon={(
               <Badge badgeContent={wishlistItems.length} color="error">
                 <FavoriteIcon sx={{ pl: '10px' }} />
@@ -87,7 +87,7 @@ const Navbar = () => {
             component={Link}
             to='/cart'
             onClick={scrollToTop}
-            sx={{ p:0.1,py:0.5,minWidth: '10px', bgcolor: 'rgba(77, 73, 73, 0.59)', boxShadow: '0px 5px 10px black', '&:hover': { bgcolor: 'rgba(40, 39, 39, 0.1)' } }}
+            sx={{ p:0.1,py:0.5,minWidth: '10px',pr:{xs:0,sm:0,md:0,lg:1}, bgcolor: 'rgba(77, 73, 73, 0.59)', boxShadow: '0px 5px 10px black', '&:hover': { bgcolor: 'rgba(40, 39, 39, 0.1)' } }}
 
             startIcon={(
               <Badge badgeContent={cartCount} color="secondary">
